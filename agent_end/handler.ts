1445db3d2d9e70c7c3aed26eda3312589dbe1181
lailai369 <15806333803@163.com>
2026-06-18 13:47:43 +0800

    v3.1.3: 添加 HOOK.md 支持新版 OpenClaw

---
+++ agent_end/handler.ts
@@ -0,0 +1,4 @@
+﻿// agent_end hook handler
+export default async function handler(ctx: any) {
+  // 由主 index.ts 统一处理
+}
---
+++ before_compaction/handler.ts
@@ -0,0 +1,4 @@
+﻿// before_compaction hook handler
+export default async function handler(ctx: any) {
+  // 由主 index.ts 统一处理
+}
